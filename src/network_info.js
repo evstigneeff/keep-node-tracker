456047f0a3fc80ad7002c4f2875b1732ef1bbcbb
Dmitriy Evstigneyev <evstigneeff@gmail.com>
2020-09-05 13:14:07 -0400

    v1.0.0

--- src/network_info.js
+++ src/network_info.js
@@ -9,11 +9,10 @@ const getTransactions = async (action, address) => {
         const txList1 = await axios(`${url}?module=account&action=${action}&address=${address}&startblock=0&endblock=99999999&sort=asc&apikey=${api}`)
 
         if (!txList1.data.result[9999]) {
-
             return txList1.data.result
         } else {
             const middleBlock = txList1.data.result[9999].blockNumber
-            const txList2 = await axios(`${url}?module=account&action=${action}&address=${address}&startblock=${middleBlock}&endblock=99999999&sort=asc&apikey=${api}`)
+            txList2 = await axios(`${url}?module=account&action=${action}&address=${address}&startblock=${middleBlock}&endblock=99999999&sort=asc&apikey=${api}`)
 
             return [...txList1.data.result, ...txList2.data.result]
         }
